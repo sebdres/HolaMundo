4e439319045374e7b8c6844ef650e4ae356e7e49
sebdres <baxardep@baxardep-PC>
2016-07-14 23:31:15 -0300

    Le agregué el mensaje hola mundo

--- HolaMundo/src/com/sebdres/sistema/Sistema.java
+++ HolaMundo/src/com/sebdres/sistema/Sistema.java
@@ -3,7 +3,8 @@ package com.sebdres.sistema;
 public class Sistema {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		System.out.println("Hola Mundo");
 
 	}
 
